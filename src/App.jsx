70e33f686f5e0639c6893b641c50fa7494f3f8ca
pearlpandz <12746886+pearlpandz@users.noreply.github.com>
2025-07-06 09:09:54 +0530

    Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthContext } from './context/auth.context';
 import { WebsocketProvider } from './utils/WebsocketProvider';
 import Home from './pages/Home/Home';
@@ -60,69 +60,71 @@ function App() {
   return (
     <WebsocketProvider>
       <AuthContext.Provider value={{ isAuthenticated, userDetails, login, logout }}>
-        <Routes>
-          {/* Public routes with sidebar banner */}
-          <Route element={<PublicRoute />}>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/otp-verification" element={<OtpVerification />} />
-            <Route path="/pan-verification" element={<PanVerification />} />
-            <Route path="/end-verification" element={<EndVerification />} />
-            <Route path="/forgot-password" element={<ForgetPassword />} />
-          </Route>
+        <Router>
+          <Routes>
+            {/* Public routes with sidebar banner */}
+            <Route element={<PublicRoute />}>
+              <Route path="/" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/otp-verification" element={<OtpVerification />} />
+              <Route path="/pan-verification" element={<PanVerification />} />
+              <Route path="/end-verification" element={<EndVerification />} />
+              <Route path="/forgot-password" element={<ForgetPassword />} />
+            </Route>
 
-          {/* Protected routes with header/sidebar */}
-          <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
-            <Route path="/dashboard" element={<Home />} />
-            <Route path="/mobile-recharge" element={<MobileRecharge />} />
-            <Route path="/bill-payment" element={<BillPayment />} />
-            <Route path="/pay-bills" element={<PayBills />} />
-            <Route path="/talkal-wallet-load" element={<TalkalWalletLoad />} />
-            <Route path="/tatkal-success" element={<TatkalSuccess />} />
-            <Route path="/settlement" element={<SettlementPayment />} />
-            <Route path="/account" element={<AccountInfo />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/retailer-bank">
-              <Route index element={<Navigate to={'/retailer-bank/list'} />} />
-              <Route path="list" element={<RetailerBankList />} />
-              <Route path="add" element={<RetailerAddBank />} />
-              <Route path="edit/:id" element={<RetailerBankList />} />
-            </Route>
-            <Route path="/wallet-transfer">
-              <Route index element={<Navigate to={'/wallet-transfer/history'} />} />
-              <Route path="history" element={<WalletTransferHistory />} />
-              <Route path="find-recipient" element={<FindRecipient />} />
-              <Route path="new" element={<NewWalletTransfer />} />
-            </Route>
-            <Route path="/balance-request">
-              <Route index element={<Navigate to={'/balance-request/history'} />} />
-              <Route path="history" element={<BalanceRequestHistory />} />
-              <Route path="new" element={<NewBalanceRequest />} />
-            </Route>
-            <Route path="/agent-reports">
-              <Route path="ledger" element={<LedgerReports />} />
-              <Route path="cashout" element={<CashoutReports />} />
-              <Route path="money-transfer" element={<MoneyTransferReports />} />
-              <Route path="money-hold" element={<MoneyTransferHold />} />
-              <Route path="bill-payment" element={<BillPaymentReports />} />
-              <Route path="payment-gateway" element={<PaymentGatewayReports />} />
-              <Route path="outstanding" element={<OutstandingReports />} />
-              <Route path="topup" element={<TopupReports />} />
-            </Route>
-            <Route path='/settings'>
-              <Route path="set-tpin" element={<SetPin />} />
-              <Route path="reset-tpin" element={<ResetPin />} />
-              <Route path="change-password" element={<ChangePassword />} />
-              <Route path="complaints">
-                <Route index element={<Navigate to={'/settings/complaints/list'} />} />
-                <Route path="list" element={<ComplaintList />} />
-                <Route path="new" element={<NewComplaint />} />
+            {/* Protected routes with header/sidebar */}
+            <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+              <Route path="/dashboard" element={<Home />} />
+              <Route path="/mobile-recharge" element={<MobileRecharge />} />
+              <Route path="/bill-payment" element={<BillPayment />} />
+              <Route path="/pay-bills" element={<PayBills />} />
+              <Route path="/talkal-wallet-load" element={<TalkalWalletLoad />} />
+              <Route path="/tatkal-success" element={<TatkalSuccess />} />
+              <Route path="/settlement" element={<SettlementPayment />} />
+              <Route path="/account" element={<AccountInfo />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/retailer-bank">
+                <Route index element={<Navigate to={'/retailer-bank/list'} />} />
+                <Route path="list" element={<RetailerBankList />} />
+                <Route path="add" element={<RetailerAddBank />} />
+                <Route path="edit/:id" element={<RetailerBankList />} />
               </Route>
+              <Route path="/wallet-transfer">
+                <Route index element={<Navigate to={'/wallet-transfer/history'} />} />
+                <Route path="history" element={<WalletTransferHistory />} />
+                <Route path="find-recipient" element={<FindRecipient />} />
+                <Route path="new" element={<NewWalletTransfer />} />
+              </Route>
+              <Route path="/balance-request">
+                <Route index element={<Navigate to={'/balance-request/history'} />} />
+                <Route path="history" element={<BalanceRequestHistory />} />
+                <Route path="new" element={<NewBalanceRequest />} />
+              </Route>
+              <Route path="/agent-reports">
+                <Route path="ledger" element={<LedgerReports />} />
+                <Route path="cashout" element={<CashoutReports />} />
+                <Route path="money-transfer" element={<MoneyTransferReports />} />
+                <Route path="money-hold" element={<MoneyTransferHold />} />
+                <Route path="bill-payment" element={<BillPaymentReports />} />
+                <Route path="payment-gateway" element={<PaymentGatewayReports />} />
+                <Route path="outstanding" element={<OutstandingReports />} />
+                <Route path="topup" element={<TopupReports />} />
+              </Route>
+              <Route path='/settings'>
+                <Route path="set-tpin" element={<SetPin />} />
+                <Route path="reset-tpin" element={<ResetPin />} />
+                <Route path="change-password" element={<ChangePassword />} />
+                <Route path="complaints">
+                  <Route index element={<Navigate to={'/settings/complaints/list'} />} />
+                  <Route path="list" element={<ComplaintList />} />
+                  <Route path="new" element={<NewComplaint />} />
+                </Route>
 
+              </Route>
             </Route>
-          </Route>
 
-        </Routes>
+          </Routes>
+        </Router>
       </AuthContext.Provider>
     </WebsocketProvider>
   );
